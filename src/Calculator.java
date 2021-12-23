a106b9fda6d8689ce45b8fee7d3f00e3045a3c86
ValeriiKorolev <kingvv63@gmail.com>
2021-12-23 23:06:02 +0300

    Change method 'devide'

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,22 @@
 import java.util.function.*;
+import java.lang.Exception;
 
 public class Calculator {
     static Supplier<Calculator> instance = Calculator::new;
 
+    Integer myInf = Integer.MAX_VALUE;
     BinaryOperator<Integer> plus = Integer::sum;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> devide = (x, y) -> {
-        if (y > 0) return x / y;
+        if (y != 0) return (x / y);
         else {
-            System.out.println("Деление на ноль запрещено!");
+            try {
+                throw new Exception("ОШИБКА: Деление на ноль!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return myInf;
         }
     };
     UnaryOperator<Integer> pow = x -> x * x;
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
         int c = calc.devide.apply(a, b);
+
         calc.println.accept(c);
 
     }
